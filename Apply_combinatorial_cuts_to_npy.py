70820ef3c9e9cb2335bab877198ffc850259a078
Alex Marshall <alex.marshall@bristol.ac.uk>
2018-10-03 16:03:14 +0100

    cut on individual track P

--- Apply_combinatorial_cuts_to_npy.py
+++ Apply_combinatorial_cuts_to_npy.py
@@ -10,7 +10,7 @@ single_background = np.load('single_muon_track_info_not1overweight.npy')
 pair_background = np.load('pair_muon_track_info_not1overweight.npy')
 
 # single_muon_track_info = np.append(single_muon_track_info, [[weight, nmeas, rchi2, P, Px, Py, Pz]], axis=0)
-# pair_muon_track_info = np.append(pair_muon_track_info, [[weight, doca, zv, dist, pair_mom]],axis=0)
+# pair_muon_track_info = np.append(pair_muon_track_info, [[weight, doca, zv, dist, pair_mom, fid, mom_i_mag, mom_j_mag]],axis=0)
 
 print(np.shape(single_background), np.shape(pair_background))
 
@@ -18,23 +18,29 @@ print(np.shape(single_background), np.shape(pair_background))
 total_rate_pre_cut = np.sum(pair_background[:,0])
 print('total rate:', total_rate_pre_cut)
 
+#doca
 to_delete = np.where(pair_background[:,1] > 1)
-
 pair_background = np.delete(pair_background, to_delete, axis=0)
 
+#ip
 to_delete = np.where(pair_background[:,3] > 250)
-
 pair_background = np.delete(pair_background, to_delete, axis=0)
 
-to_delete = np.where(pair_background[:,4] < 1)
+# to_delete = np.where(pair_background[:,4] < 1)
 
+# pair_background = np.delete(pair_background, to_delete, axis=0)
+
+#individual track mom
+to_delete = np.where(pair_background[:,6] < 1)
+pair_background = np.delete(pair_background, to_delete, axis=0)
+to_delete = np.where(pair_background[:,7] < 1)
 pair_background = np.delete(pair_background, to_delete, axis=0)
 
+
+#fiducial
 to_delete = np.where(pair_background[:,5] == 0)
-
 pair_background = np.delete(pair_background, to_delete, axis=0)
 
-#if fiducial
 
 print(np.shape(pair_background), np.sum(pair_background[:,0]), np.sum(pair_background[:,0])/total_rate_pre_cut)
 
